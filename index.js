f3478e019bbdab8c08cab5eb00715b3ed113b795
stepanchaparyan <schaparian@yahoo.com>
2019-04-19 17:06:03 +0400

    update index.js, update coverage

--- index.js
+++ index.js
@@ -132,46 +132,9 @@ module.exports = class TestRailAPIs {
 		let month, day, minute;
 		const getDay = date.getDate();
 		getDay < 10 ? (day = `0${getDay}`) : (day = getDay);
-		switch (new Date().getMonth()) {
-			case 0:
-				month = 'Jan';
-				break;
-			case 1:
-				month = 'Feb';
-				break;
-			case 2:
-				month = 'Mar';
-				break;
-			case 3:
-				month = 'Apr';
-				break;
-			case 4:
-				month = 'May';
-				break;
-			case 5:
-				month = 'Jun';
-				break;
-			case 6:
-				month = 'Jul';
-				break;
-			case 7:
-				month = 'Aug';
-				break;
-			case 8:
-				month = 'Sep';
-				break;
-			case 9:
-				month = 'Oct';
-				break;
-			case 10:
-				month = 'Nov';
-				break;
-			case 11:
-				month = 'Dec';
-				break;
-			default:
-				month = 'month';
-		}
+		const months = ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec'];
+		month = months[new Date().getMonth()];
+		
 		const year = date.getFullYear();
 		const hour = date.getHours();
 		let getMinute = date.getMinutes();
